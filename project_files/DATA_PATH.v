f5cd038fca092cd782d75d1bc9e9794351cd6a74
Jogesh Singh <98607828+jogeshsingh@users.noreply.github.com>
2022-03-11 15:43:51 +0530

    Update DATA_PATH.v

--- project_files/DATA_PATH.v
+++ project_files/DATA_PATH.v
@@ -36,7 +36,7 @@ input [`DATA_WIDTH_1-1:0] data_in ;
 output [`DATA_WIDTH_1-1:0] out_ACC_reg ;   //accumulator /
 output [`DATA_WIDTH_1-1:0] out_A_reg ;       ///reg A --multiplicand//
 output [`DATA_WIDTH_1-1:0] out_B_reg ;     //reg B --- Multiplier //
-output A_out ;                 /////lsb bit of reg  A (aka : out_A_reg ) ///  
+ output A_out ;                 /////lsb bit of reg  A (aka : out_A_reg[0] ) ///  
 output [`DATA_WIDTH_1-1:0] shift_ADD ;
 wire [`DATA_WIDTH_1-1:0] SUM ;
 wire cout;
